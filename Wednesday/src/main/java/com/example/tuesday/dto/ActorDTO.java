74fe21ff242692b356d6a6ee43a7ef0161d88058
D3JP <deimislapinas@gmail.com>
2025-02-05 14:25:26 +0200

    Add actor controller

---
+++ Wednesday/src/main/java/com/example/tuesday/dto/ActorDTO.java
@@ -0,0 +1,19 @@
+package com.example.tuesday.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record ActorDTO(
+        @NotNull
+        @Size(min = 2, max = 50, message = "Field has to be between 2 and 50 characters long.")
+        String your,
+
+        @NotNull
+        @Pattern(
+                regexp = "^[A-Z][a-z]+$",
+                message = "Liking must start with a capital letter, and" +
+                        " be followed only by (at least one) non-capital letters"
+        )
+        String liking) {
+}
---
+++ Wednesday/src/main/java/com/example/tuesday/repository/ActorRepository.java
@@ -0,0 +1,9 @@
+package com.example.tuesday.repository;
+
+import com.example.tuesday.model.Actor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ActorRepository extends JpaRepository<Actor, Long> {
+
+
+}
